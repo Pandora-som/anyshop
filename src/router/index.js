60fe36171dbcf02321e550e7b8b657521fcd2908
Mari <testirovsik0005@mail.ru>
2024-12-23 19:22:05 +0300

    изменения для github pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Catalog from '../views/CatalogPage.vue';
 import Details from '../views/GoodCard.vue';
 import Basket from '../views/BasketPage.vue';
@@ -20,7 +20,7 @@ const routes = [
 },
 ];
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
